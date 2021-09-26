00dba2ba038c71395137358f35230eb7860e216a
moodealer <bigarthas88@gmail.com>
2021-09-26 22:19:16 +0300

    1 module 1-5tasks

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  let result = 1;
+  while (n) {
+    result *= n--;
+  }
+  return result;
 }
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,16 +1,18 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if (!name) {
+    return false;
+  }
+  if (name.includes(' ')) {
+    return false;
+  }
+  if (name.length < 4) {
+    return false;
+  }
+  return true;
 }
 
 function sayHello() {
@@ -22,3 +24,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+sayHello();
