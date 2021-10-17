5daf16b5fdd5ee45268610df9dbf9ade792e0822
moodealer <bigarthas88@gmail.com>
2021-10-17 19:10:01 +0300

    3,4 modules

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ').map(Number);
+  let newArr = [];
+  let result = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  let sortedArr = newArr.sort((a, b) => a - b);
+  result.min = sortedArr[0];
+  result.max = sortedArr[sortedArr.length - 1];
+  return result;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (+row.cells[1].innerHTML < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+    if (row.cells[2].innerHTML === 'm') {
+      row.classList.add('male');
+    }
+    if (row.cells[2].innerHTML === 'f') {
+      row.classList.add('female');
+    }
+    if (row.cells[3].getAttribute('data-available') === 'true') {
+      row.classList.add('available')
+    }
+    if (row.cells[3].getAttribute('data-available') === 'false') {
+      row.classList.add('unavailable')
+    }
+    if (!row.cells[3].hasAttribute('data-available')) {
+      row.hidden = true;
+    }
+}
 }
